ed9c99e23a796d347d02978fc07e348d15ddbfdf
Robb Kane <robb.kane@appdynamics.com>
2019-09-04 15:54:55 -0700

    first draft for Rob Navarro to test with

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ SDK_INC_DIR	= $(SDK_ROOT)/include
 SDK_LIB_DIR	= $(SDK_ROOT)/lib
 
 # The derived executable
-SATURATIONTEST_TEST_EXE = saturationtest
+SATURATIONTEST_TEST_EXE = "saturationtest"
 
 # CCP	    = g++
 #CPPFLAGS   = -g -std=c++11
@@ -22,17 +22,17 @@ CFLAGS      = -g
 # includes
 SDK_INC     = -I$(SDK_INC_DIR)
 
-# For Centos7 add the following to the LDFLAGS define:   -lrt -lpthred
-LDFLAGS         = -lz -lappdynamics -ldl 
+# For Centos7 add the following to the LDFLAGS define:   -lrt 
+LDFLAGS         = -lz -lappdynamics -ldl -lpthread
 SDK_LOAD_PATH	= LD_LIBRARY_PATH=$(SDK_LIB_DIR)
 
 # Citadel basic params: at a minimum you must provide the host, port, account, and key fields.
 # Everything else will default to the values set in the code.
 # 
-CONTROLLER_HOST=4.2.2.2
-CONTROLLER_PORT=8080
-CONTROLLER_ACCESS=abcde12345
-CONTROLLER_ACCOUNT=customer1
+#CONTROLLER_HOST=4.2.2.2
+#CONTROLLER_PORT=8080
+#CONTROLLER_ACCESS=abcde12345
+#CONTROLLER_ACCOUNT=customer1
 
 SATURATIONTEST_EXE_OPTS =  $(CONTROLLER_HOST)
 SATURATIONTEST_EXE_OPTS += $(CONTROLLER_PORT)
@@ -41,7 +41,50 @@ SATURATIONTEST_EXE_OPTS += $(CONTROLLER_ACCOUNT)
 
 all:	$(SATURATIONTEST_TEST_EXE)
 
-.PHONY: clean run params
+.PHONY: clean run params help
+
+help:
+	@echo "\nThis program will generate massive amounts of custom metrics."
+	@echo "It does so via the AppDynamics C/C++ SDK, and some not so clever code."
+	@echo "To execute it, run \"make\" with no params to get the default behavior."
+	@echo "The default behavior is to create 1000 custom metrics, once a minute, for 100 minutes."
+	@echo "Or, run \"make once\" to have it generate only 1 custom metric only once."
+	@echo "  that can be useful to just see what the actual paramters used will be."
+	@echo "Or, run \"make lots\" to have it generate 10000 custom metrics for 100 minutes."
+	@echo
+	@echo "You must set the following environment variables beforehand!"
+	@echo "------------------------------------------------------------"
+	@echo "1) CONTROLLER_HOST    - the URL or ipaddr of the Controller."
+	@echo "2) CONTROLLER_PORT    - the port address of the Controller."
+	@echo "3) CONTROLLER_ACCESS  - the access key for the Controller."
+	@echo "4) CONTROLLER_ACCOUNT - the account name (typically \"customer1\")."
+	@echo 
+	@echo "The parameters must be passsed as parameters 1 through 4 on the program command line."
+	@echo "When you run the program, it will create the \"Metric_Saturation_Test_App\" Application"
+	@echo "with a Tier called \"Metric_Saturation_Tier\", and within it two Nodes, the first is"
+	@echo "called \"Default_Node\", and it will register some number of BTs to it when the test is"
+	@echo "running, indicating that load is being generated.   The second is called \"Sauturation_Node\""
+	@echo "and it will show the Custom Metrics."
+	@echo
+	@echo "The program, as it runs each cycle, will also generate some number of BTs for both the"
+	@echo "Default and Saturation Nodes, once per minute/cycle.   It generates up to 4 different BT"
+	@echo "names for the Default Node, and up to 32 different BT names for the Saturation Node".
+	@echo
+	@echo "To view metrics, after the test has fully run, use the Metric Browser, then select"
+	@echo "the \"Application Infrastructure Performance\" entry, then expand the \"Metric_Saturation_Tier\","
+	@echo "then expand the \"Custom Metrics\" entry, which will list all of the metric names.  Each"
+	@echo "metric will have a name of the form \"Metric______N\" where N represents a value equal to the"
+	@echo "metric, and the value associated with it."
+	@echo
+	@echo "The test takes two other parameters, the number of cycles to execute, and the number of metrics"
+	@echo "to create, as parameters 5 and 6.   The default values are 100 and 1000, respectively.   Each"
+	@echo "cycle will produce some numner of metrics, and then sleep for 1 minute.  The idea is that the"
+	@echo "underpinning libagent logic, which writes to the Controller on 1 minute boundaries, will continually"
+	@echo "be sending metrics and therefore fully taxing the Controller."
+	@echo 
+	@echo "Note: There isn't any known way to delete a custom metric once created, via the Controller U/I."
+	@echo "If you discover one, please notify the author, and update this Makefile help message."
+	@echo 
 
 clean:
 	@echo "cleaning everything..."
@@ -54,7 +97,13 @@ params:
 
 $(SATURATIONTEST_TEST_EXE): saturationtest.c Makefile
 	rm -f $@
-	$(CC) $(CFLAGS) $(SDK_INC) $< -L${SDK_LIB_DIR} $(LDFLAGS) -o $@
+	$(CC) $(CFLAGS) $(SDK_INC) $< -L${SDK_LIB_DIR} $(LDFLAGS) -o $@ 
 
-run:	$(SATURATIONTEST_TEST_EXE)
-	$(SDK_LOAD_PATH) ./$(SATURATIONTEST_TEST_EXE) $(SATURATIONTEST_EXE_OPTS)
+once:	$(SATURATIONTEST_TEST_EXE)
+	$(SDK_LOAD_PATH) ./$(SATURATIONTEST_TEST_EXE) ${SATURATIONTEST_EXE_OPTS} 1 1
+
+some:	$(SATURATIONTEST_TEST_EXE)
+	$(SDK_LOAD_PATH) ./$(SATURATIONTEST_TEST_EXE) ${SATURATIONTEST_EXE_OPTS} 10 10
+
+lots:	$(SATURATIONTEST_TEST_EXE)
+	$(SDK_LOAD_PATH) ./$(SATURATIONTEST_TEST_EXE) $(SATURATIONTEST_EXE_OPTS) 1000 1000
--- saturationtest.c
+++ saturationtest.c
@@ -22,19 +22,21 @@
 
 int main(int argc, char** argv)
 {
-    int            init_timeout = -1;
+    int            init_timeout = 60000;
     int            pause = 1;
     int            rc = 0;
 
     appd_bt_handle        bt_handle = NULL;
+    appd_bt_handle        saturation_bt_handle = NULL;
     struct appd_config*   cfg = NULL;
 
-    unsigned long  bt_begin_calls_made = 0;
-    unsigned long  bt_end_calls_made = 0;
-    unsigned long  limit = 10000;
-    unsigned long  loop = 0;
-    unsigned long  controller_port = 0;
-    unsigned long  proxy_port = 0;
+    unsigned long  metrics = 1000;
+    unsigned long  cycles = 10;
+    unsigned long  metric = 0;
+    unsigned long  cycle = 0;
+
+    unsigned long  controller_port = 8090;
+    unsigned long  proxy_port = 9191;
 
     unsigned       use_SSL = FALSE;
     unsigned       use_cert_file = FALSE;
@@ -43,14 +45,13 @@ int main(int argc, char** argv)
     char*          cert_file = NULL;
     char*          cert_dir = NULL;
 
-    char*          controller_host = NULL;
+    char*          controller_host = "localhost";
     char*          acct_name = "customer1";
 
-    char*          app_name = "SDKTestApp1";
-    char*          tier_name = "SDKTestTier1";
-    char*          node_name = "SDKTestNode1";
-
-    char*          bt_name = "SDKTestBT1";
+    char*          app_name = "Metric_Saturation_Test_App";
+    char*          tier_name = "Metric_Saturation_Tier";
+    char*          default_node_name = "Default_Node";
+    char*          saturation_node_name = "Saturation_Node";
 
     char*          proxy_host = NULL;
     char*          proxy_user = NULL;
@@ -58,26 +59,53 @@ int main(int argc, char** argv)
     char*          proxy_pswd_file = NULL;
 
     char*          access_key = "Access Key Not Set";
+    const char*    saturation_context = "saturation_context";
 
     if (argc > 1)
     {
+		printf("Setting Controller Host to \"%s\" (argv[1])\n", argv[1]);
 		controller_host = argv[1];
     }
 
     if (argc > 2)
     {
+		printf("Setting Controller Port to \"%s\" (argv[2])\n", argv[2]);
 		unsigned int temp = atoi(argv[2]);
 		controller_port = temp & 0xFFFF;
     }
 
     if (argc > 3)
     {
+		printf("Setting Controller Access Key to \"%s\" (argv[3])\n", argv[3]);
 		access_key = argv[3];
     }
 
     if (argc > 4)
     {
+		printf("Setting Controller Account to \"%s\" (argv[4])\n", argv[4]);
 		acct_name = argv[4];
+    }
+
+    if (argc > 5)
+    {
+		printf("Setting Cycles to \"%s\" (argv[5])\n", argv[5]);
+		cycles = atol(argv[5]);
+		if (cycles == 0 || cycles > 10000)
+		{
+			printf("Error: Cycles (%ld) exceeds range.  Setting to 1", cycles);
+			cycles = 1;
+		}
+    }
+
+    if (argc > 6)
+    {
+		printf("Setting Metrics to \"%s\" (argv[6])\n", argv[6]);
+		metrics = atol(argv[5]);
+		if (metrics == 0 || metrics > 10000)
+		{
+			printf("Error: metrics (%ld) exceeds range.  Setting to 1", metrics);
+			metrics = 1;
+		}
     }
 
     printf("Controller Wait Timeout is %d seconds\n", (init_timeout/1000)); 
@@ -86,10 +114,16 @@ int main(int argc, char** argv)
     printf("Controller useSSL is %s\n",               use_SSL ? "true" : "false");
     printf("Controller Application is \"%s\"\n",      app_name);
     printf("Controller Tier is \"%s\"\n",             tier_name);
-    printf("Controller Node is \"%s\"\n",             node_name);
+    printf("Controller Default Node is \"%s\"\n",     default_node_name);
+    printf("Controller Saturation Node is \"%s\"\n",  saturation_node_name);
+    printf("Controller Node is \"%s\"\n",             default_node_name);
     printf("Controller Account Name is \"%s\"\n",     acct_name);
     printf("Controller License is \"%s\"\n",          access_key);
 
+    printf("\n\nCycling %ld times, creating %ld Custom Metics, with 1 BT per Node created per cycle (per minute)\n", cycles, metrics); 
+	printf("\nContinuing in 5 seconds...\n");
+	sleep(5);
+	
    cfg = appd_config_init();
 
    if (cfg == NULL)
@@ -103,10 +137,11 @@ int main(int argc, char** argv)
    appd_config_set_controller_account(cfg, acct_name);
    appd_config_set_controller_access_key(cfg, access_key);
    appd_config_set_controller_use_ssl(cfg, use_SSL);
-   appd_config_set_init_timeout_ms(cfg, 10000);
    appd_config_set_app_name(cfg, app_name);
    appd_config_set_tier_name(cfg, tier_name);
-   appd_config_set_node_name(cfg, node_name);
+   appd_config_set_node_name(cfg, default_node_name);
+   appd_config_set_flush_metrics_on_shutdown(cfg, 1);
+   appd_config_set_init_timeout_ms(cfg, 10000);
 
    if (use_cert_file)
    {
@@ -123,7 +158,27 @@ int main(int argc, char** argv)
        appd_config_set_controller_http_proxy_password_file(cfg, proxy_pswd_file);
    }
 
-   printf("About to call appd_sdk_init(), waiting for Controller response...\n");
+   struct appd_context_config* saturation_cfg = appd_context_config_init(saturation_context);
+   appd_context_config_set_controller_host(      saturation_cfg, controller_host);
+   appd_context_config_set_controller_port(      saturation_cfg, controller_port & 0xFFFF);
+   appd_context_config_set_controller_account(   saturation_cfg, acct_name);
+   appd_context_config_set_controller_access_key(saturation_cfg, access_key);
+   appd_context_config_set_controller_use_ssl(   saturation_cfg, use_SSL);
+   appd_context_config_set_app_name(             saturation_cfg, app_name);
+   appd_context_config_set_tier_name(            saturation_cfg, tier_name);
+   appd_context_config_set_node_name(            saturation_cfg, saturation_node_name);
+
+   printf("About to call appd_sdk_add_app_context()...\n");
+
+   rc = appd_sdk_add_app_context(saturation_cfg);
+
+   if (rc != 0)
+   {
+       printf("appd_sdk_add_app_context() failed!\n");
+       exit(1);
+   }
+
+   printf("About to call appd_sdk_init(), waiting up to %d seconds for Controller response...\n", (init_timeout/1000));
 
    rc = appd_sdk_init(cfg);
 
@@ -133,23 +188,71 @@ int main(int argc, char** argv)
        exit(1);
    }
 
-   for (loop = 0; loop < limit; loop++)
+   printf("\nInitialization complete, now registering metrics...\n\n");
+
+    for (metric = 1; metric <= metrics; metric++)
+	{
+		char metric_path[] = "Custom Metrics|Metric_________________________________________________________________"; 
+		sprintf(&metric_path[25], "%ld%c", metric, 0);
+
+		printf("appd_custom_metric_add(\"%s\", \"%s\", %d, %d, %d)\n", saturation_context, metric_path, 
+			APPD_TIMEROLLUP_TYPE_AVERAGE, APPD_CLUSTERROLLUP_TYPE_INDIVIDUAL,  APPD_HOLEHANDLING_TYPE_RATE_COUNTER);
+
+		appd_custom_metric_add (saturation_context, metric_path, 
+			APPD_TIMEROLLUP_TYPE_AVERAGE, APPD_CLUSTERROLLUP_TYPE_INDIVIDUAL,  APPD_HOLEHANDLING_TYPE_RATE_COUNTER);
+	}
+
+   printf("\nMetric registration complete, now creating custom metrics and BTs...\n\n");
+
+   for (cycle = 1; cycle <= cycles; cycle++)
    {
-        sleep(pause);
-		printf("Calling appd_bt_begin()...");
-        bt_handle = appd_bt_begin(bt_name, NULL);
-        bt_begin_calls_made++;
-		printf("handle = %p\n", bt_handle);
-        appd_bt_end(bt_handle);
-        bt_end_calls_made++;
+
+	char default_bt_name[]    = "Default_BT__________________________________________________________________"; 
+	char saturation_bt_name[] = "Saturation_BT_______________________________________________________________"; 
+
+	sprintf(&default_bt_name[25], "%ld%c", (cycle % 4), 0);
+	sprintf(&saturation_bt_name[25], "%ld%c", (cycle % 32), 0);
+
+	printf("Calling appd_bt_begin(\"%s\", NULL)...", default_bt_name);
+    bt_handle = appd_bt_begin(default_bt_name, NULL);
+	printf("handle = %p\n", bt_handle);
+	
+	printf("Calling appd_bt_begin_with_app_context(\"%s\", \"%s\", NULL)...", saturation_context, saturation_bt_name);
+    saturation_bt_handle = appd_bt_begin_with_app_context(saturation_context, saturation_bt_name, NULL);
+	printf("handle = %p\n\n", saturation_bt_handle);
+
+    for (metric = 1; metric <= metrics; metric++)
+	{
+		char metric_path[] = "Custom Metrics|Metric_________________________________________________________________"; 
+
+		sprintf(&metric_path[25], "%ld%c", metric, 0);
+
+		appd_custom_metric_report(saturation_context, metric_path, metric);
+
+		printf("appd_custom_metric_report(\"%s\", \"%s\", %ld)\n", saturation_context, metric_path, metric);
+	}
+
+	printf("Cycle %ld Completed\n", cycle);
+
+    appd_bt_end(saturation_bt_handle);
+    appd_bt_end(bt_handle);
+	sleep(60);
+
    }
+
+    printf("\nCycles completed.\n");
+    printf("Created %ld custom metrics, ran %ld cycles.\n", metrics, cycles);
+	printf("Created %ld unique Default BTS, sent 1 BT begin/end pair per cycle.\n", (cycles < 4) ? cycles : 4);
+	printf("Created %ld unique Saturation BTS, sent 1 BT begin/end pair per cycle.\n", (cycles < 32) ? cycles : 32);
+	printf("Waiting for SDK (libagent) to finish flushing to Controller.\n");
+	printf("This can take between one and two minutes to complete...\n");
+	sleep(1);
 
     appd_sdk_term();
 
-    printf("All SDK API calls have completed.\n\n");
-    printf("BT Begin Calls Made:         %lu\n", bt_begin_calls_made);
-    printf("BT End   Calls Made:         %lu\n", bt_end_calls_made);
+    printf("\n\nAll SDK API calls have completed, process terminated nominally.\n\n");
 
+	exit(0);
 }
 
 // end of saturationtest.cpp file.
